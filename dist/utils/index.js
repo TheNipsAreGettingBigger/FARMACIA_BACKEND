918644e0dba189e1231672ef92730d8f1ee5962d
ThomRoman <thomtwd@gmail.com>
2022-12-29 00:19:55 -0500

    c

--- dist/utils/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isObjectEmpty = void 0;
-const isObjectEmpty = (obj) => Object.values(obj).every(value => value === 0);
-exports.isObjectEmpty = isObjectEmpty;
